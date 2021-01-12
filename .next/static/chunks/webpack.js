df43a4dd4d52dbc01f010fa85e9f5d5abc433f7c
amdegi0402 <planet415@gmail.com>
2021-01-12 09:07:36 +0900

    second commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7d2098468187ec85d36b";
+/******/ 	var hotCurrentHash = "c9bf86d257d70ee46ee3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
-import {NextPage} from "next";
-import Game from "../components/Game";
+import Link from 'next/link'
+import Layout from '../components/MyLayout.js'
 
-export default function Home({ allPostsData }){
-    return(
-        <Layout home>
-            <Head>
-            <title>{siteTitle}</title>
-            </Head>
-            <section className = {utilStyles.headingMd}>
-                <p>
-                    Hello, I
-                </p>
-            </section>
-        </Layout>
-    )
-}
+const PostLink = (props) =>(
+    <li>
+        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+)
+
+export default () =>(
+    <Layout>
+        <h1>My Blog</h1>
+        <ul>
+            <PostLink id="hello-nextjs" title="Hello Next.js" />
+            <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+            <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+        </ul>
+    </Layout>
+)
